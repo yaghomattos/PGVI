b6e8cc4fcba7a863f32ad331681345e035c2bec7
Yagho Mattos <mattosyagho@gmail.com>
2023-12-17 10:12:06 -0300

    feature: home frame

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
+import { IoNotificationsOutline, IoSearch } from 'react-icons/io5';
+import { LiaUserSolid } from 'react-icons/lia';
 
 export default function Home() {
   const navigate = useRouter();
@@ -48,7 +50,47 @@ export default function Home() {
         </nav>
       </div>
 
-      <div className="flex w-4/5 mr-6 my-6 rounded-3xl items-center bg-white"></div>
+      <div className="w-4/5 mr-6 my-6 rounded-3xl bg-white">
+        <div className="flex ml-16 mt-8 items-center">
+          <label className="flex w-3/6 h-12 rounded-2xl items-center bg-gray-100">
+            <IoSearch className="ml-6" size={24} />
+            <input
+              className="w-full h-full ml-4 mr-2 bg-gray-100 outline-none"
+              type="search"
+              placeholder="Pesquisar..."
+            />
+          </label>
+
+          <div className="flex ml-14">
+            <LiaUserSolid className="mr-8" size={24} />
+            <IoNotificationsOutline size={24} />
+          </div>
+        </div>
+
+        <div className="flex-auto ml-16 mt-8">
+          <h1 className="text-3xl text-medium">Cursos não terminados</h1>
+
+          <div className="flex mt-2">
+            <div className="block w-96 h-72 mr-10 rounded-2xl bg-gray-400"></div>
+
+            <div className="block w-96 h-72 rounded-2xl bg-gray-400"></div>
+          </div>
+        </div>
+
+        <div className="ml-16 mt-8">
+          <h1 className="text-3xl text-medium">Outros Cursos</h1>
+
+          <div className="flex mt-2">
+            <div className="w-24 h-40 mr-6 rounded-xl bg-gray-400"></div>
+            <div className="w-24 h-40 mr-6 rounded-xl bg-gray-400"></div>
+            <div className="w-24 h-40 mr-6 rounded-xl bg-gray-400"></div>
+            <div className="w-24 h-40 mr-6 rounded-xl bg-gray-400"></div>
+            <div className="w-24 h-40 mr-6 rounded-xl bg-gray-400"></div>
+            <div className="w-24 h-40 mr-6 rounded-xl bg-gray-400"></div>
+            <div className="w-24 h-40 rounded-xl bg-gray-400"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
