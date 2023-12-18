6054b381746bec212ee68a3b4fbfe55a8cfd2dce
Yagho Mattos <mattosyagho@gmail.com>
2023-12-17 22:50:47 -0300

    styles: full elements home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-
+import Image from 'next/image';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 
@@ -19,9 +19,69 @@ export default function Home() {
           <h1 className="text-3xl text-medium">Cursos não terminados</h1>
 
           <div className="flex mt-2">
-            <div className="block w-2/6 h-72 mr-10 rounded-2xl bg-gray-400"></div>
+            <div className="flex flex-col justify-around w-[26vw] h-72 mr-10 rounded-2xl bg-gray-400">
+              <video
+                className="absolute w-[26vw] h-72 z-0 rounded-2xl"
+                autoPlay
+                loop
+                muted
+              >
+                <source src={''} type="video/mp4" />
+              </video>
 
-            <div className="block w-2/6 h-72 rounded-2xl bg-gray-400"></div>
+              <div className="flex flex-col items-center justify-between z-10">
+                <div className="flex justify-around w-4/5">
+                  <div className="flex w-fit">
+                    <Image className="w-[3vw] h-[3vw] rounded-full" />
+                    <div className="flex-auto">
+                      <h1 className="font-bold text-white">Alguem da silva</h1>
+
+                      <h2 className="font-light text-gray-200">@alguem</h2>
+                    </div>
+                  </div>
+
+                  <div className="w-fit h-fit py-2 px-3 rounded-lg bg-orange-600">
+                    <h2 className="text-base text-white">0 min</h2>
+                  </div>
+                </div>
+
+                <div className="flex w-4/6 h-[8vh] mx-auto pl-7 justify-center items-center rounded-xl bg-white bg-opacity-5 text-medium text-white">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col justify-around w-[26vw] h-72 mr-10 rounded-2xl bg-gray-400">
+              <video
+                className="absolute w-[26vw] h-72 z-0 rounded-2xl"
+                autoPlay
+                loop
+                muted
+              >
+                <source src={''} type="video/mp4" />
+              </video>
+
+              <div className="flex flex-col items-center justify-between z-10">
+                <div className="flex justify-around w-4/5">
+                  <div className="flex w-fit">
+                    <Image className="w-[3vw] h-[3vw] rounded-full" />
+                    <div className="flex-auto">
+                      <h1 className="font-bold text-white">Alguem da silva</h1>
+
+                      <h2 className="font-light text-gray-200">@alguem</h2>
+                    </div>
+                  </div>
+
+                  <div className="w-fit h-fit py-2 px-3 rounded-lg bg-orange-600">
+                    <h2 className="text-base text-white">0 min</h2>
+                  </div>
+                </div>
+
+                <div className="flex w-4/6 h-[8vh] mx-auto pl-7 justify-center items-center rounded-xl bg-white bg-opacity-5 text-medium text-white">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
@@ -29,13 +89,62 @@ export default function Home() {
           <h1 className="text-3xl text-medium">Outros Cursos</h1>
 
           <div className="flex mt-2">
-            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400"></div>
-            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400"></div>
-            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400"></div>
-            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400"></div>
-            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400"></div>
-            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400"></div>
-            <div className="w-1/12 h-40 rounded-xl bg-gray-400"></div>
+            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 h-40 mr-6 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
+            <div className="w-1/12 h-40 rounded-xl bg-gray-400">
+              <div className="flex">
+                <div className="flex-auto w-fit">
+                  <Image className="w-[3vw] h-[3vw] rounded-full" />
+                  <h2 className="font-light text-gray-200">@alguem</h2>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </div>
